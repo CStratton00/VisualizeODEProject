fb9887180807a7d0cc76e68f3b4f84c47f01ebad
Collin Stratton <Collin.Stratton00@gmail.com>
2021-01-18 14:50:35 -0700

    submission code

---
+++ VisualizeODE.py
@@ -0,0 +1,26 @@
+# Collin Stratton
+# CST-305
+# This is my work
+
+# For this project, I am simulating an ODE for the GCU Internet Speed
+
+import numpy as np                          # import numpy for array space
+import matplotlib.pyplot as plt             # import matplotlib to graph function
+from scipy.integrate import odeint          # import scipy to use the ordinary differencial equation integral function
+
+# function dx_dt solves for the Number of Packets per Second 
+# by taking the Number of Packets times Time
+def dx_dt(x, t, k):                         # dx/dt = Number of Packets per Second
+    return k * t                            # return the value generated from k * t
+
+ts = np.linspace(0, 10, 100)                # t     = Time(Seconds)
+x = 1000                                    # x     = Number of Packets
+k = 1000                                    # k     = Initial Number of Packets
+
+xs = odeint(dx_dt, k, ts, args=(x,))        # function to find the ODE Integral of x and t
+
+# matplotlib graph displaying number of packets over time
+plt.xlabel("t")
+plt.ylabel("x")
+plt.plot(ts, xs)
+plt.show()
